4af0298f4e69a74cef4403a91c6ec0a0b81130e7
Abdul Nabi Sheeraz <sheeraz842122@gmail.com>
2025-12-06 15:46:14 +0530

    Created mavin project

---
+++ Grocery-Shop-Management-System/src/main/java/com/example/Grocery/Shop/Management/System/GroceryShopManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.Grocery.Shop.Management.System;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GroceryShopManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GroceryShopManagementSystemApplication.class, args);
+	}
+
+}
---
+++ Grocery-Shop-Management-System/src/test/java/com/example/Grocery/Shop/Management/System/GroceryShopManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Grocery.Shop.Management.System;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GroceryShopManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
